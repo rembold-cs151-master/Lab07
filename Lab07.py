471d23b72ea27a01f3f7ed5a9b077148496b36f9
Jed Rembold <jedediah.r@gmail.com>
2020-09-09 12:22:30 -0700

    Add initial contents

---
+++ Lab07.py
@@ -0,0 +1,40 @@
+# Filename: Lab07.py
+
+"""
+Lab to practice using functions and the
+random library.
+"""
+
+import random
+
+# Define your roll_dice function
+
+
+
+
+
+
+
+
+
+
+
+
+# Define your play_game function
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    play_game(50)
+    # play_game(50,2)
+    # play_game(500,5,10)
+
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Lab07
-Contents for Lab 7
+# Lab 7
+Today you are practicing writing two functions to play a simple dice-based game. Follow along in the instructions to write out both functions.
+
+Owing to the random nature of the dice, writing auto-tests for this is difficult, so there will likely not be any.
